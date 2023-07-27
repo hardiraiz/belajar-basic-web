ffde10777291b833b7e3a88c49f933c3d53d96fc
Hardi Raiz <raizhardi@gmail.com>
2023-07-27 10:43:45 +0700

    backup data

---
+++ ibox-iphone/assets/product.js
@@ -0,0 +1,7 @@
+function updateColor(value) {
+    document.querySelector('#warna').innerHTML = value;
+}
+
+function updateStorage(value) {
+    document.querySelector('#penyimpanan').innerHTML = value;
+}
